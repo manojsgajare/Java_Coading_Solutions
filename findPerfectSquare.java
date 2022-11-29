05a41cd7a860a77f722ede81076f2f070deb39f3
akotakr <31746420+akotakr@users.noreply.github.com>
2022-11-29 19:59:52 +0530

    updated

---
+++ findPerfectSquare.java
@@ -0,0 +1,16 @@
+
+
+public class findPerfectSquare {
+    // FINDING SQUARE ROOT
+
+    public static void main(String[] args) {
+      
+      int num = 25;
+
+      for (int i=0;i<=num/2;i++){
+        if ((i*i) == num){
+            System.out.println(i);
+        }
+      }
+    }
+}

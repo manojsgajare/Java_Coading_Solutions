fb41912dec2ebe6df9d4e5d8494571f046709cef
manoj <98097534+manojsgajare@users.noreply.github.com>
2022-12-06 20:03:33 +0530

    static class

---
+++ javaStaticKeyword/staticMethods.java
@@ -0,0 +1,32 @@
+package javaStaticKeyword;
+
+// Look at the errors in the below program
+// Uncomment the lines
+
+
+/*
+ * When a method declared with the static keyword, it is known as static method.
+ * The most common example of the static method is main method.
+ */
+public class staticMethods {
+
+    static int a = 10;
+
+    int b = 20;
+    static void m1() {
+        a = 20;
+        System.out.println("FROM M1");
+  //      b = 10;
+  //      m2();
+  //      System.out.println(super.a);
+    }
+    void m2() {
+        System.out.println("from m2");
+    }
+
+    public static void main(String[] args) {
+        
+
+
+    }
+}

4fb38cf1cbab102486b7dd8e5740e9dbecd3a71f
manoj <98097534+manojsgajare@users.noreply.github.com>
2022-12-21 21:07:16 +0530

    21/12/2022

--- Strings/detectCapital.java
+++ Strings/detectCapital.java
@@ -2,26 +2,26 @@ package Strings;
 
 // leetcode: 520. Detect Capital
 
-class detectCapital{
+class detectCapital {
 
-    public boolean detectCapitalUse(String word) 
-    {
-         word = word.trim();
-        for(int i = 0; i < word.length(); i++) {
-            if(word.charAt(i) >= 'A' && word.charAt(i) <= 'Z') {
-                if(i > 0 && (word.charAt(i-1) < 'A' || word.charAt(i-1) > 'Z')) {
+    public boolean detectCapitalUse(String word) {
+        word = word.trim();
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) >= 'A' && word.charAt(i) <= 'Z') {
+                if (i > 0 && (word.charAt(i - 1) < 'A' || word.charAt(i - 1) > 'Z')) {
                     return false;
                 }
-            } else if(word.charAt(i) >= 'a' && word.charAt(i) <= 'z') {
-                if(i > 1 && (word.charAt(i-1) < 'a' || word.charAt(i-1) > 'z')) {
+            } else if (word.charAt(i) >= 'a' && word.charAt(i) <= 'z') {
+                if (i > 1 && (word.charAt(i - 1) < 'a' || word.charAt(i - 1) > 'z')) {
                     return false;
                 }
-            } 
+            }
         }
         return true;
     }
+
     public static void main(String[] args) {
-        
+
         String word = "ABC";
 
         detectCapital dct = new detectCapital();

3b89b54cc8956dc5c025dea1c0192d9accbb53f8
manoj <98097534+manojsgajare@users.noreply.github.com>
2022-12-21 21:11:29 +0530

    21/12/2022

--- Constructor/callFromAnother.java
+++ Constructor/callFromAnother.java
@@ -1,33 +1,28 @@
 package Constructor;
 
+class callFromAnother {
 
-class callFromAnother{
-
-
-     int sum;
+    int sum;
 
     // First Constructor
-    callFromAnother(){
-        this(5 ,2);
+    callFromAnother() {
+        this(5, 2);
     }
 
-
     // Second Constructor
-    callFromAnother(int arg1, int arg2){
+    callFromAnother(int arg1, int arg2) {
         this.sum = arg1 + arg2;
     }
 
     // Display Sum
-    void display(){
+    void display() {
         System.out.println("Sum is : " + sum);
     }
 
-
     public static void main(String[] args) {
-        
+
         // Call first class consttructor
         callFromAnother obj = new callFromAnother();
-
 
         // Display the method
         obj.display();

1b47022c8edfa68eb267528e5b7401646b3edc5a
manoj <98097534+manojsgajare@users.noreply.github.com>
2022-12-21 21:08:14 +0530

    21/12/2022

--- factorial.java
+++ factorial.java
@@ -1,17 +1,17 @@
 public class factorial {
 
-    public static int fact(int number){
-    int result=1;
-    
-    while(number !=0){
-        result *= number;
-        number--;
-    }
+    public static int fact(int number) {
+        int result = 1;
+
+        while (number != 0) {
+            result *= number;
+            number--;
+        }
 
         return result;
     }
-    
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         int num = 5;
         System.out.println(fact(num));
     }

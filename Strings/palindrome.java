30f42749825bd173fd738763b4ca4e1dd3754881
manoj <98097534+manojsgajare@users.noreply.github.com>
2022-12-27 00:33:52 +0530

    27/12/2022

---
+++ Strings/palindrome.java
@@ -0,0 +1,41 @@
+package Strings;
+
+public class palindrome {
+
+    public static boolean checkPalindrome(char[] ch) {
+
+        int flag = 1;
+
+        if (ch.length < 2) {
+            return false;
+        }
+
+        for (int i = 0, j = ch.length - 1; i < ch.length / 2; i++, j--) {
+
+            if (ch[i] == ch[j]) {
+                
+            }
+            else{
+                flag =0;
+            }
+
+        }
+
+        if (flag == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        String s = "abcba";
+
+        s.toLowerCase();
+        char[] ch = s.toCharArray();
+
+        System.out.println(checkPalindrome(ch));
+    }
+
+}

d53bbc871e6de05a53a1ea8bb96ae78eb56b8a99
manoj <98097534+manojsgajare@users.noreply.github.com>
2023-01-02 18:52:51 +0530

    02/01/2023

---
+++ Pattern/downwardPyramid.java
@@ -0,0 +1,17 @@
+package Pattern;
+
+public class downwardPyramid {
+
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = n - i; j >= 1; j--) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+}
